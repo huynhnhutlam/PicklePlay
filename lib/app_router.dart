84f36f2e8f2782ee4d2d9021b6a6e62c80405a02
Huỳnh Nhựt Lâm <nhutlam@bincorporation.com>
2025-07-28 08:31:10 +0700

    update

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-import 'package:get_it/get_it.dart';
 import 'package:pickle_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:pickle_app/features/auth/presentation/bloc/auth_event.dart';
 import 'package:pickle_app/features/auth/presentation/pages/login_page.dart';
